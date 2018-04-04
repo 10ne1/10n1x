d191bb72d53f0d87582eb57818815e14cd33d20b
Ioan-Adrian Ratiu <adi@adirat.com>
2018-04-05 00:03:03 +0300

    init-env: remove hackery, add proper init
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- init-env.sh
+++ init-env.sh
@@ -2,10 +2,6 @@ TEMPLATECONF=${TEMPLATECONF:-$PWD/meta/conf}
 BITBAKEDIR=${BITBAKEDIR:-$PWD/bitbake}
 BUILDDIR=${BUILDDIR:-$PWD/build}
 
-if [ ! -f $BUILDDIR/conf/bblayers.conf ]; then
-    sed "s?##PWD##?$PWD?g" $TEMPLATECONF/bblayers.conf.sample > $BUILDDIR/conf/bblayers.conf
-fi
-
 export PS1="(bb) $PS1"
 export TEMPLATECONF
 source $PWD/layers/openembedded-core/oe-init-build-env
