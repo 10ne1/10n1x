1bff3d441abf7af386e9dbedb9258ddae20b3100
Ioan-Adrian Ratiu <adi@adirat.com>
2018-05-21 14:13:24 +0300

    scw1play.sh: give input in nanoseconds
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- layers/meta-10n1x/recipes-multimedia/scw1play/scw1play/scw1play.sh
+++ layers/meta-10n1x/recipes-multimedia/scw1play/scw1play/scw1play.sh
@@ -2,13 +2,9 @@
 
 audio_formats="flac\|mp3\|wv" #etc
 
-channels=2
-samplerate=192000
-sampleformat=S32_LE
-
 alsadevice=hw:0,0
 hw_buffer=65536
-loops_per_second=10000
+wakeup_nsec=125000
 
 tmpdir="/tmp/playhrt"
 
@@ -43,12 +39,12 @@ for path in "$@"; do
 
 	pipe-size $tmpdir/unpack_fifo 2097152 & # 2^21 ~ 2mb
 
-	taskset -c 1,2 chrt -f 60 resample_soxr -i $(sox --i -r "$file") -o $samplerate \
+	taskset -c 1,2 chrt -f 60 resample_soxr -i $(sox --i -r "$file") -o 192000 \
 	     <$tmpdir/unpack_fifo >$tmpdir/repack_fifo &
 
 	pipe-size $tmpdir/repack_fifo 2097152 & # 2^21 ~ 2mb
 
-	taskset -c 1,2 chrt -f 70 sox -t raw -e float -b 64 -c $channels -r $samplerate \
+	taskset -c 1,2 chrt -f 70 sox -t raw -e float -b 64 -c 2 -r 192000 \
 	     $tmpdir/repack_fifo -e signed -b 32 -t raw $tmpdir/playhrt_fifo
     done
 done &
@@ -56,9 +52,7 @@ done &
 pipe-size $tmpdir/playhrt_fifo $((2**24)) & # 2^24 ~ 16 mb
 
 taskset -c 3 chrt -f 99 playhrt < $tmpdir/playhrt_fifo \
-     -SMNvv \
+     -v \
      -d $alsadevice \
-     -s $samplerate \
-     -f $sampleformat \
-     -n $loops_per_second \
+     -n $wakeup_nsec \
      -c $hw_buffer
