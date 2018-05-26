f89b1155d33bda620719312a6ccee2650a118666
Ioan-Adrian Ratiu <adi@adirat.com>
2018-05-27 00:26:05 +0300

    scw1play.sh: refactor for small buffer sizes
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- layers/meta-10n1x/recipes-multimedia/scw1play/scw1play/scw1play.sh
+++ layers/meta-10n1x/recipes-multimedia/scw1play/scw1play/scw1play.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-audio_formats="flac\|mp3\|wv" #etc
+audio_formats="flac\|mp3\|wv\|ogg" #etc
 
 alsadevice=hw:0,0
-hw_buffer=65536
+hw_buffer=16384 #65536
 wakeup_nsec=125000
 
 tmpdir="/tmp/playhrt"
@@ -35,24 +35,28 @@ for path in "$@"; do
     for file in $audio_files; do
 	[ -f $tmpdir/do_exit ] && exit 0
 
-	taskset -c 1,2 chrt -f 50 sox "$file" -t raw -e float -b 64 $tmpdir/unpack_fifo &
+	fsrate=$(sox --i -r "$file")
 
-	pipe-size $tmpdir/unpack_fifo 2097152 & # 2^21 ~ 2mb
+	pipe-size $tmpdir/unpack_fifo $((2**29)) & # 2^29 ~ 536 mb
 
-	taskset -c 1,2 chrt -f 60 resample_soxr -i $(sox --i -r "$file") -o 192000 \
-	     <$tmpdir/unpack_fifo >$tmpdir/repack_fifo &
+	taskset -c 1,2 chrt -f 60 sox "$file" -t raw -e float -b 64 $tmpdir/unpack_fifo &
 
-	pipe-size $tmpdir/repack_fifo 2097152 & # 2^21 ~ 2mb
+	pipe-size $tmpdir/repack_fifo $((2**23)) & # 2^23 ~ 8 mb
 
-	taskset -c 1,2 chrt -f 70 sox -t raw -e float -b 64 -c 2 -r 192000 \
-	     $tmpdir/repack_fifo -e signed -b 32 -t raw $tmpdir/playhrt_fifo
+	taskset -c 1,2 chrt -f 70 resample_soxr -i $fsrate -o 192000 \
+		<$tmpdir/unpack_fifo >$tmpdir/repack_fifo &
+
+	taskset -c 1,2 chrt -f 80 sox -t raw -e float -b 64 -c 2 -r 192000 \
+		$tmpdir/repack_fifo -e signed -b 32 -t raw $tmpdir/playhrt_fifo &
+
+	wait
     done
 done &
 
-pipe-size $tmpdir/playhrt_fifo $((2**24)) & # 2^24 ~ 16 mb
+pipe-size $tmpdir/playhrt_fifo $((2**24)) # 2^24 ~ 16 mb
 
 taskset -c 3 chrt -f 99 playhrt < $tmpdir/playhrt_fifo \
-     -v \
      -d $alsadevice \
      -n $wakeup_nsec \
-     -c $hw_buffer
+     -c $hw_buffer \
+     -D 2000000
