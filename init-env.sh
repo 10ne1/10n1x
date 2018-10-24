58eb8f4d56d1c3f50d0102a2b8a91fa59f119d7a
Ioan-Adrian Ratiu <adi@adirat.com>
2018-10-24 20:27:07 +0000

    init-env.sh: fix feed server redirection order
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- init-env.sh
+++ init-env.sh
@@ -7,6 +7,6 @@ export TEMPLATECONF
 source $PWD/layers/openembedded-core/oe-init-build-env
 
 kill "$(ps aux | grep -i python3 | head -n1 | awk '{print $2}')" 2>&1 >/dev/null
-pushd tmp-musl/deploy/ipk 2>&1 >/dev/null
-python3 -m http.server 2>&1 >/dev/null &
-popd 2>&1 >/dev/null
+pushd tmp-musl/deploy/ipk >/dev/null 2>&1
+python3 -m http.server >/dev/null 2>&1 &
+popd >/dev/null 2>&1
