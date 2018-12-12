6fc936e2574841417d98ef68cd0ab099919b9d87
Ioan-Adrian Ratiu <adi@adirat.com>
2018-12-12 20:07:54 +0000

    init-env: remve python server (now handled by nginx)
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- init-env.sh
+++ init-env.sh
@@ -5,8 +5,3 @@ BUILDDIR=${BUILDDIR:-$PWD/build}
 export PS1="(bb) $PS1"
 export TEMPLATECONF
 source $PWD/layers/openembedded-core/oe-init-build-env
-
-kill "$(ps aux | grep -i python3 | head -n1 | awk '{print $2}')" 2>&1 >/dev/null
-pushd tmp-musl/deploy/ipk >/dev/null 2>&1
-python3 -m http.server >/dev/null 2>&1 &
-popd >/dev/null 2>&1
